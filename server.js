25863569ef50850d3fe0b23c2c9e7247a613469e
jasondu <duguangwei@youzan.com>
2017-07-29 17:45:55 +0800

    add client

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { 
+  graphqlExpress, 
+  graphiqlExpress, 
+} from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
+import cors from 'cors';
 
-const GRAPHQL_PORT = 3000;
+const GRAPHQL_PORT = 3002;
 
 const graphQLServer = express();
 
+graphQLServer.use('*', cors({ origin: 'http://localhost:51404' })); // 注意：这里要修改成客户端的端口
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
